4f203f8f2433925c2359c9d3b7bec89a2acf26c2
John Ovalle <johnovalle@gmail.com>
2017-12-13 23:49:18 -0500

    intermediate commit, setup call to api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "John", "lastName": "Ovalle", "email": "johnovalle@gmail.com"},
+    {"id": 2, "firstName": "Human", "lastName": "Person", "email": "humanperson@gmail.com"},
+    {"id": 3, "firstName": "Robot", "lastName": "Sentinel", "email": "robotsentinel@gmail.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
